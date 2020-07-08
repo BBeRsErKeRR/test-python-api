02450bf6298840f2c39d7ec52aee6d736ef8048d
BBeRsErKeRR <ddarkwatchh@mail.ru>
2020-07-09 03:03:26 +1000

    add tests sample

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from app import create_app
+
+@pytest.yield_fixture(scope='session')
+def _app():
+    _app = create_app(None, None)
+    app_context = _app.app_context()
+    app_context.push()
+
+    yield _app
+
+    app_context.pop()
+
+
+@pytest.fixture()
+def client(app)
