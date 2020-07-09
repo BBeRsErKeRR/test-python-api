f84134a16754b90b428df8751ee5a6fee45f8ee6
BBeRsErKeRR <ddarkwatchh@mail.ru>
2020-07-09 21:55:41 +1000

    fix error from test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 class TestApi(object):
 
     def test_hello_world(self, client):
-        res = client.get('api/hello')
+        res = client.get('/api/hello')
         assert res.status_code == 200
         assert 'hello world' in str(res.data)
