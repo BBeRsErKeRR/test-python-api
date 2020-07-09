65f479ae2eb40af17122bc70873cf715688c0816
BBeRsErKeRR <ddarkwatchh@mail.ru>
2020-07-09 19:19:44 +1000

    fix flake

--- g_config.py
+++ g_config.py
@@ -2,7 +2,7 @@ import os
 
 bind = "0.0.0.0:5000"
 
-workers = os.environ.get('WORKERS',1)
+workers = os.environ.get('WORKERS', 1)
 
 keepalive = 2
 errorlog = '-'
--- logging_config.py
+++ logging_config.py
@@ -1,6 +1,5 @@
 import os
-import jsonlogging
-from app import __verxion__
+#import jsonlogging
 
 LOG_FORMATER = os.environ.get('LOG_FORMATER', 'default')
 LOGLEVEL = os.environ.get('LOGLEVEL', 'info')
